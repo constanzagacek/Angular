6fb6e769d6283916b21e23c79dc7bc6547201768
constanzagacek <constanzaGV008@gmail.com>
2025-09-25 17:01:58 -0300

    Muchos cambios

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from "./pages/contact/contact";
+import { Products } from "./pages/products/products";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Contact, Home, Products],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
