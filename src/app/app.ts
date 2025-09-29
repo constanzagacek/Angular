ea6623f39ee040928a43ee87748b52f717ff13b8
constanzagacek <constanzaGV008@gmail.com>
2025-09-29 17:13:24 -0300

    Cambios de estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
-import { Contact } from "./pages/contact/contact";
-import { Products } from "./pages/products/products";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Contact, Home, Products],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('nuevoProyecto');
 }
