dc3c6f83519cbd292c24dd534598e00aa91ddae2
constanzagacek <constanzaGV008@gmail.com>
2025-09-30 17:04:57 -0300

    Cambis en home. Estilo y contenido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { Products } from './pages/products/products';
 import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
+    /*Definimos la ruta general o la raíz en especifico que nos lleve a la ruta HOME -> principal*/
+    {path: "**", component: Home},
     {path: "home", component: Home},
     {path: "about", component: About},
     {path: "products", component: Products},
