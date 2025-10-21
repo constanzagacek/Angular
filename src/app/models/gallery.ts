16b86ab325897fe4df3c9b845310b00af1803154
constanzagacek <constanzaGV008@gmail.com>
2025-10-21 16:50:45 -0300

    Aspectos de la página

--- src/app/models/gallery.ts
+++ src/app/models/gallery.ts
@@ -1,12 +1,12 @@
-/*ng g i nombreInterfaz*/
+/*ng g i carpeta/nombreInterfaz o nombreInterfaz unicamente*/
 /*ng generate interface nombreInterfaz*/
 
 /*Interfaz de tarjeta: Es una estructura esperada con su tipo de dato.*/
-export interface Card {
+
+export interface Gallery {
     id: string; // Números con letras (ej.: 123AB / 123 / ABC)
     img: string;
     alt: string;
     titulo: string;
-    descrip: string;
-    fechaPub: number;
 }
+
